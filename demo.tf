4d88cd64bf5aa4adf92acdf5bb73b8c757228c44
Amlan Das <dasamlan271988@gmail.com>
2025-07-17 10:30:42 +0530

    added

--- demo.tf
+++ demo.tf
@@ -20,7 +20,7 @@ variable "number" {
 output "num_op" {
   value = var.number
 }
-
+#working on list variables
 variable "list_type" {
     default = [
         "Amlan",
@@ -33,4 +33,18 @@ variable "list_type" {
 
 output "list_output" {
   value = "My name is ${var.list_type[0]} and my birth place is ${var.list_type[2]} and my age is ${var.list_type[1]} and iam bad is ${var.list_type[3]}"
+}
+
+#Working on mapped variables
+
+variable "map_variable" {
+  default = {
+    "machine" = "redhat",
+    "uptime" = 1000,
+    "app_owner" = "Amlan",
+    "patch_update" = false
+  }
+}
+output "map_output" {
+  value = "I have a ${var.map_variable["machine"]} machine with an uptime of ${var.map_variable["uptime"]} and the app owner is ${var.map_variable["app_owner"]} and patching status ${var.map_variable["patch_update"]}}"
 }
