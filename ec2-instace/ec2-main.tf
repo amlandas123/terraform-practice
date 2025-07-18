62fb55318b8b226ab85527e95eccdeff52b4a61b
Amlan Das <dasamlan271988@gmail.com>
2025-07-18 23:20:18 +0530

    added

--- ec2-instace/ec2-main.tf
+++ ec2-instace/ec2-main.tf
@@ -9,5 +9,10 @@ resource "aws_instance" "dasa" {
 
 resource "aws_ebs_volume" "my_volume" {
     availability_zone    = "us-east-1a"
+    size              = 100
+    type              = "gp3"
+    tags = {
+      name: "dasa-volume" 
+    }
   # Leave this block empty for now
 }
