8de958936957df8b08b1a1fae66e639f53542b56
Amlan Das <dasamlan271988@gmail.com>
2025-07-18 23:21:34 +0530

    added

--- ec2-instace/ec2-main.tf
+++ ec2-instace/ec2-main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "dasa" {
     ami = "ami-0fcc78c828f981df2"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
 
     tags = {
         name = "Hello-World-instance"
