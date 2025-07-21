089300f3d620e9aec066d2285fedbba3247095da
Amlan Das <dasamlan271988@gmail.com>
2025-07-21 20:46:47 +0530

    added

--- ec2-instace/ec2-main.tf
+++ ec2-instace/ec2-main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dasa" {
-    ami = "ami-0fcc78c828f981df2"     ## arguements
+    ami = "ami-0fcc78c828f981df2"     # arguements
     instance_type = "t3.micro"
     vpc_security_group_ids = ["aws_security_group.amlan-web_sg.id"]
 
@@ -43,13 +43,13 @@ resource "aws_security_group" "amlan-web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # egress {
-  #   description = "Allow all outbound traffic"
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"  # All protocols
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "amlan-web-sg"
