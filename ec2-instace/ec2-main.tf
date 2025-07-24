e1460dd0405e8490cb6fd59d0d5b47a5400750a4
Amlan Das <dasamlan271988@gmail.com>
2025-07-24 12:19:12 +0530

    added

--- ec2-instace/ec2-main.tf
+++ ec2-instace/ec2-main.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "dasa" {
+resource "aws_instance" "dasa-amlan" {
     ami = "ami-0fcc78c828f981df2"     # arguements
     instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.amlan-web_sg.id]
+    vpc_security_group_ids = [aws_security_group.amlan-web_sg2025.id]
 
     tags = {
-        name = "Hello-World-instance"
+        name = "dasa-instance"
     }
 } 
 
-resource "aws_ebs_volume" "my_volume" {
+resource "aws_ebs_volume" "my_volume1" {
     availability_zone    = "us-east-1a"
     size              = 100
     type              = "gp3"
@@ -19,18 +19,18 @@ resource "aws_ebs_volume" "my_volume" {
 }
 
 output "instance_id" {
-  value = aws_instance.dasa.id   ## attributes
+  value = aws_instance.dasa-amlan.id   ## attributes
 }
 
-resource "aws_security_group" "amlan-web_sg" {
+resource "aws_security_group" "amlan-web_sg2025" {
   name        = "web-sg"
   description = "Allow HTTP and SSH"
   #vpc_id      = aws_vpc.main_vpc.id  # Use your VPC ID if already existing
 
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
