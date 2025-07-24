813439095e5dbb01648b6a6a2c558b9ec7b2d21b
Amlan Das <dasamlan271988@gmail.com>
2025-07-24 15:16:24 +0530

    added

--- modules/locals/sg/main.tf
+++ modules/locals/sg/main.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "amlan-web_sg2025" {
 
   ingress {
     description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
