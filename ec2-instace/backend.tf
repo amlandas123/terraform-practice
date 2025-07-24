0ea53e79dcd1eba9601de323f559f4e63df67058
Amlan Das <dasamlan271988@gmail.com>
2025-07-24 12:46:01 +0530

    added

--- ec2-instace/backend.tf
+++ ec2-instace/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "amlan-terraform-state-bucket"
     key            = "basics/terraform.tfstate"
     region         = "ap-southeast-2"
-    #dynamodb_table = "terraform-lock-table"  # optional: for state locking
-    #encrypt        = true
+    dynamodb_table = "terraform-lock-table"  # optional: for state locking
+    encrypt        = true
   }
 }
