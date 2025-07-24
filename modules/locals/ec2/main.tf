753a6c39d9fdf91d01af12e2abd4ee82ec99b19c
Amlan Das <dasamlan271988@gmail.com>
2025-07-24 15:06:42 +0530

    added

--- modules/locals/ec2/main.tf
+++ modules/locals/ec2/main.tf
@@ -7,7 +7,19 @@ resource "aws_instance" "dasa-amlan" {
     ami = "ami-0fcc78c828f981df2"     # arguements
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.sg2025]
-} 
+
+    tags = {
+      Name = "dasa-demo-instance"
+    }
+
+    provisioner "local-exec" {
+      command = <<EOF
+        sleep 30
+        cd /home/devops
+        touch welcome2025.txt
+      EOF
+    }
+  } 
 
 # output "public_ip" {
 #   value = aws_instance.dasa-amlan.public_ip
