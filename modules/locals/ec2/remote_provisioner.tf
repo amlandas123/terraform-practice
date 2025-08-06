d2081c374a99403816fbb22d4def729e66c5a4ab
Amlan Das <dasamlan271988@gmail.com>
2025-08-06 12:48:42 +0530

    added

--- modules/locals/ec2/remote_provisioner.tf
+++ modules/locals/ec2/remote_provisioner.tf
@@ -1,16 +1,16 @@
-resource "null_resource" "playbook" {
-   depends_on = [ aws_instance.dasa-amlan ]
-}
+# resource "null_resource" "playbook" {
+#    depends_on = [ aws_instance.dasa-amlan ]
+# }
 
-    connection {
-    type     = "ssh"
-    user     = "redhat"
-    password = "DevOps123"
-    host     = private_ip
-    }
+#     connection {
+#     type     = "ssh"
+#     user     = "redhat"
+#     password = "DevOps123"
+#     host     = private_ip
+#     }
 
-    provisioner "remote-exec" {
-        touch "/tmp/dasafile.txt"
-    }
+#     provisioner "remote-exec" {
+#         touch "/tmp/dasafile.txt"
+#     }
 
-}
+# }
